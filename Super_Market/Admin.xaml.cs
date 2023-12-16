b8a171527511be74ea1ead5ea07e5f1169c67623
NaderMamdouh99 <Nader.mamdouh9898@gmail.com>
2023-12-16 18:19:11 +0200

    Add project files.

---
+++ Super_Market/Admin.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Super_Market.Models;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Super_Market
+{
+    /// <summary>
+    /// Interaction logic for Admin.xaml
+    /// </summary>
+    public partial class Admin : UserControl
+    {
+        #region connect to database
+
+        Context Context = new Context();
+        public string UserName = "";
+        public Admin(string name)
+        {
+            InitializeComponent();
+            UserName = name;
+            UserWelcome_Label.Content = "  Welcome " + name + "!";
+        }
+        #endregion
+
+        #region props of admin
+        //Store
+        private void ListViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+            stores.Content = new Store();
+        }
+        //Category
+        private void list_2_Selected(object sender, RoutedEventArgs e)
+        {
+            stores.Content = new Category();
+        }
+        //Product
+        private void ListViewItem_Selected_1(object sender, RoutedEventArgs e)
+        {
+            stores.Content = new Product();
+        }
+        //Sales
+        private void ListViewItem_Selected_2(object sender, RoutedEventArgs e)
+        {
+            stores.Content = new Sales(UserName);
+        }
+        //Purchase
+        private void ListViewItem_Selected_3(object sender, RoutedEventArgs e)
+        {
+            stores.Content = new Purchase();
+        }
+        //Report
+        //private void ListViewItem_Selected_4(object sender, RoutedEventArgs e)
+        //{
+        //    stores.Content = new Report();
+        //}
+        //Retrieval
+        private void ListViewItem_Selected_5(object sender, RoutedEventArgs e)
+        {
+            stores.Content = new Retrieval();
+
+        }
+        //MainWindow
+        private void ListViewItem_Selected_6(object sender, RoutedEventArgs e)
+        {
+
+            MainWindow window = new MainWindow();
+            window.Show();
+            Window.GetWindow(this).Close();
+        }
+        //Supplier
+        private void ListViewItem_Selected_7(object sender, RoutedEventArgs e)
+        {
+            stores.Content = new Supplier();
+        }
+        //Minimized
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+            window.WindowState = WindowState.Minimized;
+        }
+        //Maximized
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            Window window = Window.GetWindow(this);
+            if (window.WindowState == WindowState.Maximized)
+            {
+                window.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                window.WindowState = WindowState.Maximized;
+            }
+        }
+        //Shutdown
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+    }
+
+}
---
+++ Super_Market/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using Super_Market.Models;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Super_Market
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Context context = new Context();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            #region select box
+
+            string UserRole = comboValue.Text;
+            if (UserRole != "Select Role")
+            {
+
+                if (UserRole == "Admin")
+                {
+
+                    var AdminQuery = context.users.Where(usr => usr.Pos == (position)1 && usr.IsDelete == false && usr.UserName == txtUsrName.Text && usr.Password == txtPass.Password).FirstOrDefault();
+
+                    if (AdminQuery != null)
+                    {
+                        Login.Content = new Admin(txtUsrName.Text);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"ooops!! you entered wrong email or password please enter a valid data for Admin", "Wrong Data Input", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+
+                }
+
+                else if (UserRole == "Casher")
+                {
+                    var casherQuery = context.users.Where(usr => usr.Pos == 0 && usr.UserName == txtUsrName.Text && usr.Password == txtPass.Password).FirstOrDefault();
+                    if (casherQuery != null)
+                    {
+                        Login.Content = new Casher(txtUsrName.Text);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"ooops!! you entered wrong email or password please enter a valid data for casher", "Wrong Data Input", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show($"ooops!! you entered wrong user role", "please choose user role", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+            #endregion
+
+
+        }
+
+
+        private void Login_PreviewKeyDown(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void Login_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string admin = comboValue.Text;
+
+                if (admin == "Admin")
+                {
+                    var AdminQueryVaild = context.users.Where(usr => usr.Pos == (position)1 && usr.UserName == txtUsrName.Text && usr.Password == txtPass.Password).FirstOrDefault();
+                    Login.Content = new Admin(txtUsrName.Text);
+                }
+
+                else if (admin == "Casher")
+                {
+                    var casherQuery = context.users.Where(usr => usr.Pos == 0 && usr.IsDelete == false && usr.UserName == txtUsrName.Text && usr.Password == txtPass.Password).FirstOrDefault();
+                    Login.Content = new Casher(txtUsrName.Text);
+                }
+                else
+                {
+                    MessageBox.Show("wrong");
+                }
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
